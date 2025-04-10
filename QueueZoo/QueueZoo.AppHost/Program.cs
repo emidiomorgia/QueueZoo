fcc570cad87b1f39ab7c3452fa12145d7c3d86cc
Emidio Morgia <emidio@morgia.info>
2025-04-10 22:43:14 +0200

    services setup

--- QueueZoo/QueueZoo.AppHost/Program.cs
+++ QueueZoo/QueueZoo.AppHost/Program.cs
@@ -1,5 +1,12 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
+var rabbitMq = builder.AddRabbitMQ("eventbus")
+    .WithManagementPlugin() // Abilita e configura la Web UI
+       .WithEnvironment("RABBITMQ_DEFAULT_USER", "guest")
+       .WithEnvironment("RABBITMQ_DEFAULT_PASS", "guest")
+        .WithLifetime(ContainerLifetime.Persistent);
+
 builder.AddProject<Projects.ServiceM1>("servicem1");
+builder.AddProject<Projects.ServiceM2>("servicem2");
 
 builder.Build().Run();
